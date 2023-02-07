530b1db92664e9ea923ff137ac18923b39ad9789
megghyloggiu <124669237+megghyloggiu@users.noreply.github.com>
2023-02-07 12:02:23 +0100

    Add files via upload

---
+++ carmine.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+   int a;
+   int b;
+
+printf("inserisci due numeri: /n");
+scanf("%d %d", &a , &b);
+printf("divisione: %d\n", a / b);
+printf(" resto divisione: %d\n", a % b);
+
+
+
+
+
+
+}
