06ceb6eda8f7da67cc0a324e564ceda98135440b
megghyloggiu <124669237+megghyloggiu@users.noreply.github.com>
2023-02-08 09:48:37 +0100

    Add files via upload

---
+++ anni.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+int a;
+printf("inserisci un numero\n");
+scanf("%d", &a);
+
+if ((a%4==0 && a%100!=0) || a%400==0)
+{
+    printf("anno bisestile\n");
+}
+else 
+{
+    printf("anno non bisestile\n");
+}
+
+
+
+}
